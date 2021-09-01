782bd71f0e59423abaad7f40ed3f71a17e3e7074
PcsaDev <66335345+picsacoder@users.noreply.github.com>
2021-09-01 15:11:33 -0500

    :3

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-
+require('dotenv').config();
 
 app.use( express.static('public') )
 
@@ -17,4 +17,4 @@ app.get('*', (req, res) => {
 });
 
  
-app.listen(8080)
+app.listen(process.env.PORT)
